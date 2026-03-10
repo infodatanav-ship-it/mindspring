8b7bf1e98119b9d446079f9dca9e01b0563e0b70
Bevan Paulse <info.datanav@gmail.com>
2026-03-10 17:21:33 +0200

    Refactor JavaScript functions and update success/error message links in SMS form

--- include/sms.php
+++ include/sms.php
@@ -7,36 +7,36 @@ require_once('sql.php.inc');
 <title>Mindspring SMS Portal</title>
 <meta name='author' value='Marius Pese' />
 <SCRIPT LANGUAGE="JavaScript">
-<!-- Original:  Ronnie T. Moore -->
-<!-- Web Site:  The JavaScript Source -->
-<!-- Begin
-function textCounter(field, countfield, maxlimit) {
-if (field.value.length > maxlimit) // if too long...trim it!
-field.value = field.value.substring(0, maxlimit);
-// otherwise, update 'characters left' counter
-else 
-countfield.value = maxlimit - field.value.length;
-}
-// End -->
+  <!-- Original:  Ronnie T. Moore -->
+  <!-- Web Site:  The JavaScript Source -->
+  <!-- Begin
+  function textCounter(field, countfield, maxlimit) {
+  if (field.value.length > maxlimit) // if too long...trim it!
+  field.value = field.value.substring(0, maxlimit);
+  // otherwise, update 'characters left' counter
+  else 
+  countfield.value = maxlimit - field.value.length;
+  }
+  // End -->
 </script>
 
 <SCRIPT LANGUAGE="JavaScript">
-<!--
+  <!--
 
-<!-- Begin
-function Check(chk)
-{
-if(document.myform.Check_ctr.checked==true){
-for (i = 0; i < chk.length; i++)
-chk[i].checked = true ;
-}else{
+  <!-- Begin
+  function Check(chk)
+  {
+  if(document.myform.Check_ctr.checked==true){
+  for (i = 0; i < chk.length; i++)
+  chk[i].checked = true ;
+  }else{
 
-for (i = 0; i < chk.length; i++)
-chk[i].checked = false ;
-}
-}
+  for (i = 0; i < chk.length; i++)
+  chk[i].checked = false ;
+  }
+  }
 
-// End -->
+  // End -->
 </script>
 
 </head>
@@ -85,10 +85,10 @@ echo "<u> Hello " . $myname . ", please select your recipients:</u><br />";
 ?>
 </table>
 	
-<input type='text' name='recipient[]' value=''> Custom number<br /><br />
-<textarea name=message wrap=physical cols=50 rows=4 onKeyDown='textCounter(this.form.message,this.form.remLen,160);' onKeyUp='textCounter(this.form.message,this.form.remLen,160);' onfocus='if(this.value==this.defaultValue){this.value='';}' onblur='if(this.value==''){this.value=this.defaultValue;}'>Please enter your message here</textarea>
+<input type="text" name="recipient[]" value=""> Custom number<br /><br />
+<textarea name="message" wrap="physical" placeholder="Please enter your message here" cols="50" rows="4" onKeyDown='textCounter(this.form.message,this.form.remLen,160);' onKeyUp='textCounter(this.form.message,this.form.remLen,160);' onfocus='if(this.value==this.defaultValue){this.value='';}' onblur='if(this.value==''){this.value=this.defaultValue;}'>Please enter your message here</textarea>
 <br>
-<input readonly type=text name=remLen size=3 maxlength=3 value='160'> characters left</font>
+<input readonly type="text" name="remLen" size="3" maxlength="3" value="160"> characters left</font>
 <br />
 <br />
 Alright, please <input type='submit' value='send this message' name='send'>
@@ -132,11 +132,11 @@ $data= array(
 //This contains data that you will send to the server.
 $data = http_build_query($data); //builds the post string ready for posting
 echo do_post_request('https://www.mymobileapi.com/api5/http5.aspx', $data);
-echo "<p>Message successfully sent to: ". $value."<br /><a href='http://dev.mindspring.co.za/?'>Take me home</a>'</p>";
+echo "<p>Message successfully sent to: ". $value."<br /><a href='http://devintranet.mindspring.local/index.php?module=sms'>Take me home</a>'</p>";
 /*unset($value);
 }
 else{
-echo "<p>Send message failed: " . $value."<br /><a href='http://dev.mindspring.co.za/?'>Take me home</a>'</p>";
+echo "<p>Send message failed: " . $value."<br /><a href='http://devintranet.mindspring.local/?'>Take me home</a>'</p>";
 }	
 unset($value);
 }*/
